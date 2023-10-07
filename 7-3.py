393ebcac5e3db252c4ca02715643f89ef37a2218
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2023-10-07 12:54:29 +0900

    fix the fomula

--- 7-3.py
+++ 7-3.py
@@ -2,13 +2,21 @@
 
 print('勇者の攻撃力とゴーレムのHP、守備力を入力してください。')
 
-offense = int(input('勇者の攻撃力:'))
-hp = int(input('ゴーレムのHP:'))
-defense = int(input('ゴーレムの防御力:'))
+try:
+    offense = int(input('勇者の攻撃力:'))
+    hp = int(input('ゴーレムのHP:'))
+    defense = int(input('ゴーレムの防御力:'))
+except ValueError:
+    print('無効な入力です。')
+    exit()
 
 decision = True
 while decision:
-    option = int(input('[0]たたかう/[1]にげる:'))
+    try:
+        option = int(input('[0]たたかう/[1]にげる:'))
+    except ValueError:
+        print('無効な入力です。')
+        continue
     if option == 1:
         print('勇者はにげだした！')
         decision = False
@@ -28,5 +36,11 @@ while decision:
         decision = False
     else:
         print(f'ゴーレムのHP:{hp}')
-    
+
+
+"""
+入力に関する例外処理の実行を補足している
+intで受けているので文字が入ったときはValueErrorを返す
+optionでは1がかえったときにFalseで処理を終わらせる
+"""
 
