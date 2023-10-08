c0385bfe6e74fa2ef13ee1737e30438e19330551
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2023-10-08 16:51:37 +0900

    section11 completed

---
+++ section11/11-4.py
@@ -0,0 +1,38 @@
+#ans
+
+def swap(party1, party2):
+    flag = 0
+    
+    for name, hp in party2.items():
+        name2 = name
+        hp2 = hp
+    for name, hp in party1.items():
+        name1 = name
+        hp1 = hp
+        if hp1 >= hp2:
+            party1[name1] = hp2
+            flag = 1
+        
+    
+    return flag
+
+party1 = {'勇者':32, '魔法使い':14, '僧侶':22}
+party2 = {'ゴースト':15}
+
+for name, hp in party1.items():
+    print(f'{name}のHP:{hp}')
+    
+for name, hp in party2.items():
+    print(f'{name}のHP:{hp}')
+    
+print('ゴーストが現れた！')
+print('ゴーストはスワップを唱えた！')
+
+flag = swap(party1, party2)
+if(flag == 0):
+    print('何もおこらなかった！')
+if(flag == 1):
+    print('勇者たちのHPが入れ替わってしまった!')
+    for name, hp in party1.items():
+        print(f'{name}のHP:{hp}')
+    
