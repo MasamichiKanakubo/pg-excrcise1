57b3a0ce45a501a2f8b4e3193fc92bc4abd25a4a
MasamichiKanakubo <renlijinjiubao808@gmail.com>
2023-10-27 14:29:30 +0900

    17-2 unfinished

--- section17(adv)/17-2.py
+++ section17(adv)/17-2.py
@@ -6,19 +6,24 @@ O: 15G
 *をNかOに置き換えて置き換えた分だけ請求する
 """
 
-palindrome = input('なんでも回文にします。入力してください:')
+print('なんでも回文にします。入力してください')
+palindrome = input('入力:')
 n_count = 0
 o_count = 0
 for idx, string in enumerate(palindrome):
-    if string == palindrome[-idx-1] == '*':
+    if palindrome[idx] == palindrome[-idx-1] == '*':
+        x = palindrome.replace(string, 'N')
+        print(x)
+        n_count += 1
+    elif palindrome[idx] == '*' and palindrome[-idx-1] == 'N':
         x = palindrome.replace(string, 'N')
         n_count += 1
-    elif string == '*' and palindrome[-idx-1] == 'N':
-        x = palindrome.replace(string, 'N')
-        n_count += 1
-    elif string == '*' and palindrome[-idx-1] == 'O':
+        print(x)
+    elif palindrome[idx] == '*' and palindrome[-idx-1] == 'O':
         x = palindrome.replace(string, 'O')
+        print(x)
         o_count += 1
     
-print(x)
-print(f'n:{n_count*10}G, o:{o_count*15}Gで合計{n_count*10+o_count*15}です')
+print(f'出力:{x}')
+print(f'n:{n_count}, o:{o_count}')
+print(f'{n_count*10+o_count*15}Gになります')
